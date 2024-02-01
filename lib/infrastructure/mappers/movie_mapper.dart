c66f2429f4df6a768ced9fcda9b800a426901d8f
jsalasl <jsalasl@findep.com>
2024-02-01 01:56:24 -0600

    datasource e infrestructura de datos

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBtoEntity(MovieMovieDB moviedb) => Movie(
+      adult: moviedb.adult,
+      backdropPath: (moviedb.backdropPath != '')
+      ? 'https://image.tmdb.org/t/p/w500/${moviedb.backdropPath}'
+      :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2E_Z5bWXkFYh6lzmgZ50QIKOdyk3kupptew&usqp=CAU',
+      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: (moviedb.posterPath != '')
+      ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
+      :'no-poster',
+      releaseDate: moviedb.releaseDate,
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount);
+}
